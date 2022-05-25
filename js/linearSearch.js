5c207ef60cad0f272548e04be9ce80f008062492
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-25 14:53:04 +0200

    one solved

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,20 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
-}
+// const arrayToSearchThrough = [];
+// for (let i = 1; i <= 1000; i++) {
+//     arrayToSearchThrough.push(i);
+// }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+exports.linearSearch = function(valueToFind, arrayToSearchThrough) { 
+    const counter = []
+    for (let i = 0; i <= arrayToSearchThrough.length; i++){
+        if (arrayToSearchThrough[i] === valueToFind){
+            counter.push(i)
+        }
+    }
+    
+    if (counter.length === 0) {
+        return undefined
+    } else {
+        return counter[0]
+    }
 };
 
